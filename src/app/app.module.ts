8748c13ddca2e3f39a8614330d98f747b3a3ad14
ldorle <lucasdorle@hotmail.fr>
2022-06-23 11:19:24 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EnTeteComponent } from './en-tete/en-tete.component';
+import { ZonePrincipaleComponent } from './zone-principale/zone-principale.component';
+import { ZoneDeGaucheComponent } from './zone-de-gauche/zone-de-gauche.component';
+import { ZoneDuBasComponent } from './zone-du-bas/zone-du-bas.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnTeteComponent,
+    ZonePrincipaleComponent,
+    ZoneDeGaucheComponent,
+    ZoneDuBasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
